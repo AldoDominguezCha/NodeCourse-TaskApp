0823d16c217d5e9da77d00f73da2f152e86e7f05
Aldo Dominguez <nonexistant@outlook.com>
2020-11-27 22:52:57 -0600

    Prcaticed the find documents operation

--- mongodbReadPractice.js
+++ mongodbReadPractice.js
@@ -16,15 +16,15 @@ MongoClient.connect(connectionURL, {useUnifiedTopology : true}, (error, client) 
         console.log(result)
     })
     //Looking for a document providing its idObject, we cant just pass the id string,
-    //we need to create an idObject with it
-    usersCollection.findOne({ _id : ObjectID('5fc1a1970a1a273a044bf522') }, (error, result) => {
+    //we need to create a new idObject with it
+    usersCollection.findOne({ _id : new ObjectID('5fc1a1970a1a273a044bf522') }, (error, result) => {
         assert.strictEqual(null, error)
         console.log(result)
     })
     const tasksCollection = db.collection('tasks')
     //Fetching multiple documents with one query, we get back a cursor (pointer in the DB), 
     //not an array of the found documents, we need to further manipulate that cursor,
-    //here we just get the count of the documents that cursor contains
+    //here we just get the count of the documents that the cursor contains
     tasksCollection.find({ completed : true }).count((error, count) => {
         assert.strictEqual(null, error)
         console.log(`Matching documents found with the find query: ${count}.`)
